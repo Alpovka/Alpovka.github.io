32f812d34235dfa858002b060e5dbe047ccaaa3a
Alpovka <karavelx@gmail.com>
2023-01-30 02:04:17 +0300

    Integrate redux & redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+const { wrapper } = require("../redux/store");
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, ...rest }: AppProps) {
+  const { store } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <Component {...rest.pageProps} />
+    </Provider>
+  );
 }
