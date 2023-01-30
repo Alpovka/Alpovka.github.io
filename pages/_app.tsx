8f33181ff270d86afa7da7aaa225d617de3f9ef1
Alpovka <67004083+Alpovka@users.noreply.github.com>
2023-01-30 21:52:15 +0300

    Integrate Google Analytics tool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 const { wrapper } = require("../redux/store");
+import Script from "next/script";
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store } = wrapper.useWrappedStore(rest);
   return (
-    <Provider store={store}>
-      <Component {...rest.pageProps} />
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-J4XHY45C5E', {page_path: window.location.pathname,});`,
+        }}
+      />
+      <Provider store={store}>
+        <Component {...rest.pageProps} />
+      </Provider>
+    </>
   );
 }
