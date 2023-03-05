e065fda5fb1bb653694951a98da7ef355dd60d38
Alpovka <67004083+Alpovka@users.noreply.github.com>
2023-03-05 23:04:44 +0300

    Minimize skills 10%

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,7 +10,7 @@ function Skills({}: Props) {
   const { isMobile } = useDeviceDetect();
   return (
     <div className="flex relative h-screen flex-col text-center md:text-left max-w-[2000px] xl:px-10 min-h-screen justify-center space-y-16 mx-auto items-center">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center scale-90">
         <h3 className="tracking-[10px] sm:tracking-[20px] uppercase text-gray-500 text-2xl pb-5">
           Skills
         </h3>
@@ -20,7 +20,7 @@ function Skills({}: Props) {
         </h3>
       </div>
 
-      <div className="grid grid-cols-4 gap-4 px-10">
+      <div className="grid grid-cols-4 gap-4 px-10 scale-90">
         {SKILLS.slice(0, SKILLS.length / 2).map((skill, key) => (
           <SkillCricle key={key} skill={skill} />
         ))}
